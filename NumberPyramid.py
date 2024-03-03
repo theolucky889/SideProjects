be8a02f05b311093f8be6eab8261d21daa404905
theolucky889 <theolucky889@gmail.com>
2024-03-04 01:19:19 +0800

    Update NumberPyramid.py

--- NumberPyramid.py
+++ NumberPyramid.py
@@ -5,3 +5,14 @@
  1 3 3 1
     """
 #Pascal Triangle ^^^
+
+def pascal_triangle(rows):
+
+    for i in range(rows):
+        n = 1
+        for j in range(i + 1):
+            print(n, end=" ")
+            n = n * (i - j) // (j + 1)
+        print()
+
+pascal_triangle(4)
