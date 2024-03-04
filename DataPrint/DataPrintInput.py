715c77c340208554ae86a75e4816e2c796098c2b
theolucky889 <theolucky889@gmail.com>
2024-03-05 00:11:30 +0800

    Update DataPrintInput.py

---
+++ DataPrint/DataPrintInput.py
@@ -0,0 +1,14 @@
+"""
+Enter pet information and find the oldest and youngest one
+- Create a category to store attributes: pet name, age, coat color
+- Stored in array
+- After user completes input, find the oldest and youngest age
+EX = 
+Pet name, the oldest one is 12 years old
+Pet name, the youngest is 5 years old
+
+How to do:
+1. save the names, ages and coat colors of the three pets and print
+2. Find the oldest and youngest pet in the array
+    """
+    
