350e11086d10650f5c2b21b51b5e1804db848e6a
theolucky889 <theolucky889@gmail.com>
2024-03-05 00:15:15 +0800

    Update DataPrintInput.py

--- DataPrint/DataPrintInput.py
+++ DataPrint/DataPrintInput.py
@@ -12,3 +12,15 @@ How to do:
 2. Find the oldest and youngest pet in the array
     """
     
+pet_name_1 = str(input("Enter First Pet Name: "))
+pet_age_1 = float(input("Enter First Pet Age: "))
+pet_color_1 = str(input("Enter First Pet Color: "))
+
+pet_name_2 = str(input("Enter Second Pet Name: "))
+pet_age_2 = float(input("Enter Second Pet Age: "))
+pet_color_2 = str(input("Enter Second Pet Color: "))
+
+pet_name_3 = str(input("Enter Third Pet Name: "))
+pet_age_3 = float(input("Enter Third Pet Age: "))
+pet_color_3 = str(input("Enter Third Pet Color: "))
+
