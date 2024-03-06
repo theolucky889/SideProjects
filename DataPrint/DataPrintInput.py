b0d82d8ca4e9e38a6b0ec5cdebf8e1c9e164a461
theolucky889 <theolucky889@gmail.com>
2024-03-06 15:14:03 +0800

    Update DataPrintInput.py

--- DataPrint/DataPrintInput.py
+++ DataPrint/DataPrintInput.py
@@ -16,26 +16,26 @@ pets = []
 
 # pet 1
 pet_name_1 = input("Enter First Pet Name: ")
-pet_age_1 = float(input("Enter First Pet Age: "))
+pet_age_1 = int(input("Enter First Pet Age: "))
 pet_color_1 = input("Enter First Pet Color: ")
 pets.append({"name": pet_name_1, "age": pet_age_1})
 
 # pet 2
 pet_name_2 = input("Enter Second Pet Name: ")
-pet_age_2 = float(input("Enter Second Pet Age: "))
+pet_age_2 = int(input("Enter Second Pet Age: "))
 pet_color_2 = input("Enter Second Pet Color: ")
 pets.append({"name": pet_name_2, "age": pet_age_2})
 
 # pet 3
 pet_name_3 = input("Enter Third Pet Name: ")
-pet_age_3 = float(input("Enter Third Pet Age: "))
+pet_age_3 = int(input("Enter Third Pet Age: "))
 pet_color_3 = input("Enter Third Pet Color: ")
 pets.append({"name": pet_name_3, "age": pet_age_3})
 
 # Finding the Oldest and Youngest pet
-oldest_pet = max(["age"])
-youngest_pet = min(["age"])
+oldest_pet = max(pets, key=lambda x: x["age"])
+youngest_pet = min(pets, key=lambda x: x["age"])
 
 # Print oldest and youngest pet
-print(oldest_pet["name"], "The oldest one is", oldest_pet["age"], "years old")
-print(youngest_pet["name"], "The youngest one is", youngest_pet["age"], "years old")
+print(oldest_pet["name"], ",", "The oldest one is", oldest_pet["age"], "years old")
+print(youngest_pet["name"], ",", "The youngest one is", youngest_pet["age"], "years old")
