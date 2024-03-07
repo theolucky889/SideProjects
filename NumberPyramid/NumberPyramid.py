9c830e5647cd9d4a7e5ae8a4eb40a1fa4e21229d
theolucky889 <theolucky889@gmail.com>
2024-03-08 00:14:09 +0800

    Update NumberPyramid.py

--- NumberPyramid/NumberPyramid.py
+++ NumberPyramid/NumberPyramid.py
@@ -1,11 +1,11 @@
 """
-    1
-   1 1
-  1 2 1
- 1 3 3 1
+    1       a
+   1 1      b
+  1 2 1     c
+ 1 3 3 1    d
     """
 #Pascal Triangle ^^^
-
+"""
 times=25
 
 for i in range(times):
@@ -16,7 +16,7 @@ for i in range(times):
         print(n, end=" ")           # Spacing between numbers
         n = n * (i - j) // (j + 1)  # Simplified Pascal triangle formula (n k) = n!/k!-(n-k)!
     print()
-
+"""
 
 
 
@@ -29,3 +29,33 @@ Pascal triangle formula = (n k) = n!/k!-(n-k)!
 2. spacing
 3. function(15 line limit)
     """
+
+a1 = "1"
+
+b1 = "1"
+b2 = "1"
+
+c1 = "1"
+c2 = "2"
+c3 = "1"
+
+d1 = "1"
+d2 = "3"
+d3 = "3"
+d4 = "1"
+
+    
+a = " " + a1 + "\n"
+b = " " + b1 + " " + b2 + "\n"
+c = " " + c1 + " " + c2 + " " + c3 + "\n"
+d = " " + d1 + " " + d2 + " " + d3 + " " + d4 + "\n"
+
+print (a+b+c+d)
+
+
+times = input("Pascal Triangle Input = ")
+
+for i in range(times):
+    n = 1
+    n = n * (i - j) // (j + 1)
+        
