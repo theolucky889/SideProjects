2678714823436fbab9a7a6aa9de72d9bb1578396
theolucky889 <theolucky889@gmail.com>
2024-03-13 14:32:37 +0800

    Update DataPrintInput.py

--- DataPrint/DataPrintInput.py
+++ DataPrint/DataPrintInput.py
@@ -51,30 +51,30 @@ class Pet:
     def __init__(self, name, age, color):
         self.name = name
         self.age = age
-        self.clor = color
+        self.color = color
 class PetList:
     def __init__(self):
-        self.pet = []
+        self.pets = []
     
     def add_pet(self, pet):
-        self.pet.append(pet)
+        self.pets.append(pet)
     
     def age_difference(self):
-        oldest_pet = max(pet, key=lambda x: x.age)
-        youngest_pet = min(pet, key=lambda x: x.age)
+        oldest_pet = max(self.pets, key=lambda x: x.age)
+        youngest_pet = min(self.pets, key=lambda x: x.age)
         return oldest_pet, youngest_pet
 
 pet_lists = PetList()
 
 for i in range(3):
     pet_name = input(f"Enter Pet {i+1} Name: ")
-    pet_age = input(f"Enter Pet {i+1} Age: ")
+    pet_age = int(input(f"Enter Pet {i+1} Age: "))
     pet_color = input(f"Enter Pet {i+1} Color: ")
     pet = Pet(pet_name, pet_age, pet_color)
     pet_lists.add_pet(pet)
 
 oldest_pet, youngest_pet = pet_lists.age_difference()
 
-print(f"{oldest_pet.name}, The oldest one is {oldest_pet.age} years old")
-print(f"{youngest_pet.name}, The oldest one is {youngest_pet.age} years old")    
+print(f"Name: {oldest_pet.name} Color: {oldest_pet.color}, The oldest one is {oldest_pet.age} years old")
+print(f"{youngest_pet.name} Color: {youngest_pet.color}, The youngest one is {youngest_pet.age} years old")    
         
