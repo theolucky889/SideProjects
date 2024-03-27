108d2e1e4f607ca557ab8c2ccea3dc31b2fe8a10
theolucky889 <theolucky889@gmail.com>
2024-03-27 16:10:23 +0800

    Update summingmath.py

--- summingmath/summingmath.py
+++ summingmath/summingmath.py
@@ -1,1 +1,16 @@
+class Number:
+    def __init__(self, number):
+        self.number = number
+    
+    def number_root(self):
+        result = self.number
+        while result > 9:
+            result = sum(int(digit) for digit in str(result))
+        return result
+    
+user_input = input('Enter a positive number: ')
+number = int(user_input)
 
+number_result = Number(number)
+result = number_result.number_root()
+print(result)
